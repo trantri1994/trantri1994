6f01507f8c757df50c4918cab9a63714193ae4eb
trantri1994 <98171591+trantri1994@users.noreply.github.com>
2022-01-23 19:56:23 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -16,7 +16,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 echo "How many proxy do you want to create? Example 500"
 read COUNT
 
-FIRST_PORT=11000
+FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
